c4c2ad36dcb58e1d58aa7320fcee9d4c15617e1e
Virgarakha <virgakumara@gmail.com>
2025-07-25 09:39:19 +0700

    okee

--- dummy.js
+++ dummy.js
@@ -1,3 +1,5 @@
 const user = [
-    {nama: 'handoko', }
+    {nama: 'handoko', email: 'hadoko@gmail.com', password: '123456', role: 'peserta'},
+    {nama: 'anasmaulana', email: 'anasmaulana@gmail.com', password: '123456', role: 'peserta'},
+    {nama: 'admin', email: 'admin@gmail.com', password: 'admin123', role: 'admin'}
 ]
